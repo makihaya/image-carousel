12b612aba1dd689c4110cc6d471dedf93abf3325
makihaya <93355988+makihaya@users.noreply.github.com>
2022-09-06 17:48:17 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,47 @@
+const red = document.getElementById("image1");
+const blue = document.getElementById("image2");
+const yellow = document.getElementById("image3");
+const redText = document.getElementById("red-text");
+const blueText = document.getElementById("blue-text");
+const yellowText = document.getElementById("yellow-text");
+const dot1 = document.getElementById("dot1");
+const dot2 = document.getElementById("dot2");
+const dot3 = document.getElementById("dot3");
+
+let imagesArr = [image1, image2, image3];
+let textArr = [redText, blueText, yellowText];
+let dotsArr = [dot1, dot2, dot3];
+
+function carousel(direction){
+
+  if(direction == "right"){
+    imagesArr.push(imagesArr.shift());
+    textArr.push(textArr.shift());   
+    dotsArr.push(dotsArr.shift());
+  }else if(direction == "left"){
+    imagesArr.unshift(imagesArr.pop());
+    textArr.unshift(textArr.pop());
+    dotsArr.unshift(dotsArr.pop());
+  }
+    imagesArr[0].classList.remove("hide");
+    imagesArr[0].classList.add("fade-in");
+    
+    imagesArr[1].classList.add("hide");
+    imagesArr[1].classList.remove("fade-in");
+  
+    imagesArr[2].classList.add("hide");
+    imagesArr[2].classList.remove("fade-in");
+
+    textArr[0].classList.remove("hide");
+    textArr[0].classList.add("fade-in");
+
+    textArr[1].classList.add("hide");
+    textArr[1].classList.remove("fade-in");
+
+    textArr[2].classList.add("hide");
+    textArr[2].classList.remove("fade-in");
+
+  dotsArr[0].classList.remove("translucent");
+  dotsArr[1].classList.add("translucent");
+  dotsArr[2].classList.add("translucent");
+}
